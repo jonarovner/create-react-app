d86bda99b7f39c9eacd7cc3595f25b20b524d30c
Jonathan Rovner <jonathan.rovner@okta.com>
2022-06-11 15:36:27 -0300

    added auth0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hola From App
-    </div>
-  );
+
+  const {
+    isLoading,
+    isAuthenticated,
+    error,
+    user,
+    loginWithRedirect,
+    logout
+  } = useAuth0()
+
+  if (isLoading){
+    return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isAuthenticated) {
+    return (
+      <div>
+        Hello {user.name}{' '}
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+          Log out
+        </button>
+      </div>
+    );
+  } else {
+    return <button onClick={loginWithRedirect}>Log in</button>;
+  }
 }
 
 export default App;
